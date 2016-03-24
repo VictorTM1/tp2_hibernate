3416f67dce824959f0363f746adbcb716344bebb
Istvan SZALAÏ <szalai972@gmail.com>
2016-03-23 23:27:37 -0400

    update

--- src/Jsonizer.java
+++ src/Jsonizer.java
@@ -100,21 +100,26 @@ public class Jsonizer {
     }
 
     public static void loadObservations(ArrayList<String> array, Terrain terrain) {
-        if (TestObservation.testDateMesured(terrain)) {
-            array.add("\"L’écart maximal entre les dates de mesure des lots d’un même terrain devrait être de moins de 6 mois\"\n");
-        } else if (TestObservation.testFonciereValue(terrain)) {
+        if (TestObservation.testFonciereValue(terrain)) {
             array.add("\"La valeur foncière totale ne doit pas dépasser 300000.00 $.\"\n");
-        } else if (TestObservation.testSchoolTaxe(terrain)) {
+        }
+        if (TestObservation.testSchoolTaxe(terrain)) {
             array.add("\"La taxe scolaire payable par le propriétaire ne doit pas dépasser 500.00 $. Vous devez effectuer deux versements sont nécessaires pour le payement\"\n");
-        } else if (TestObservation.testSquareMeterPrice(terrain)) {
+        }
+        if (TestObservation.testSquareMeterPrice(terrain)) {
             array.add("\"Le prix maximum du m2 ne peut pas dépasser deux fois le prix minimum du m2.\"\n");
-        } else if (TestObservation.testTaxeMunicipal(terrain)) {
+        }
+        if (TestObservation.testTaxeMunicipal(terrain)) {
             array.add("\"La taxe municipale payable par le propriétaire ne doit pas dépasser 1000.00 $. Vous devez effectuer deux versements sont nécessaires pour le payement\"\n");
-        } else if (TestObservation.getTheWrongLotSurfaceMessage(terrain) != null) {
+        }
+        if (TestObservation.getTheWrongLotSurfaceMessage(terrain) != null) {
             array.add(TestObservation.getTheWrongLotSurfaceMessage(terrain));
-        } else if (TestObservation.getTheWrongLotValueMessage(terrain) != null) {
+        }
+        if (TestObservation.getTheWrongLotValueMessage(terrain) != null) {
             array.add(TestObservation.getTheWrongLotValueMessage(terrain));
         }
+        //if (TestObservation.testDateMesured(terrain)) {
+        //    array.add("\"L’écart maximal entre les dates de mesure des lots d’un même terrain devrait être de moins de 6 mois\"\n");
+        //}
     }
-
 }
